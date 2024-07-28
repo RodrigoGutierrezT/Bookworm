bb7831c7db981468d21ba47c6f955906fa65539a
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-28 17:10:53 -0400

    Initial commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Rodrigo on 28-07-24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
